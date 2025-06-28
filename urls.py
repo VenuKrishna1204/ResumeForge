cb0e603ce6ffae340a1cdfe4602804fd8d68cd86
GADDAM VENU <gaddamvenu12042004@gmail.com>
2025-06-28 19:14:51 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,25 @@
-# resume/urls.py
+"""
+URL configuration for ResumeForge project.
 
-from django.urls import path
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# ResumeForge/urls.py
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('generate_resume/', views.generate_resume, name='generate_resume'),
+    path('admin/', admin.site.urls),
+    path('', include('resume.urls')),
 ]
